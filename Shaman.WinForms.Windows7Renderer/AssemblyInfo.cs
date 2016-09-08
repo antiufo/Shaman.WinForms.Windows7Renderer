66d0e72d7a6e871ad098cc91b57234d0d89b232e
Andrea Martinelli <andmartinelli@gmail.com>
2016-09-08 10:54:14 +0200

    Initial commit

---
+++ Shaman.WinForms.Windows7Renderer/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/********************************************************************/
+/*  Office 2007 Renderer Project                                    */
+/*                                                                  */
+/*  Use the Office2007Renderer class as a custom renderer by        */
+/*  providing it to the ToolStripManager.Renderer property. Then    */
+/*  all tool strips, menu strips, status strips etc will be drawn   */
+/*  using the Office 2007 style renderer in your application.       */
+/*                                                                  */
+/*   Author: Phil Wright                                            */
+/*  Website: www.componentfactory.com                               */
+/*  Contact: phil.wright@componentfactory.com                       */
+/********************************************************************/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Windows7Renderer")]
+[assembly: AssemblyDescription("Windows7Renderer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrea Martinelli http://at-my-window.blogspot.com/")]
+[assembly: AssemblyProduct("Windows7Renderer")]
+[assembly: AssemblyCopyright("Copyright © 2010 Andrea Martinelli")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("CE294F27-6C08-4F02-9BEA-DD55901A4002")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
